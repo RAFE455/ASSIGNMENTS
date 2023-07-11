2aea482087e16d3650680091dc0670083c39b81f
RAFE455 <139132815+RAFE455@users.noreply.github.com>
2023-07-11 22:45:32 +0530

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,16 @@
+#continue 
+#for letter in 'Rafe Bagadia':
+	#if letter == 'e' or letter == 'a':
+#		continue
+#	print('Current Letter :', letter)
+
+#for letter in 'Rafe Bagadia':
+ 
+   
+  #  if letter == 'e' or letter == 'a':
+  #      break
+#print('Current Letter :', letter)
+
+for letter in 'Rafe Bagadia':
+    pass
+print('Last Letter :', letter)
